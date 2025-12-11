d2d1ab87b4a2398e66d0fc341179e4d189877a73
gypsy0 <asimaltun4@gmail.com>
2025-12-11 18:55:11 +0300

    lyver-camp

--- installation/item-vorp.sql
+++ installation/item-vorp.sql
@@ -1,4 +1,6 @@
 INSERT INTO `items` 
 (`item`, `label`, `limit`, `can_remove`, `type`, `usable`, `useExpired`, `groupId`, `metadata`, `desc`, `degradation`, `weight`) 
 VALUES 
-('camp_kit', 'Traveler Camp', 1, 1, 'item_standard', 1, 0, 1, '{}', 'Portable camp kit.', 0, 5);
+('small_camp_kit', 'Traveler Camp', 1, 1, 'item_standard', 1, 0, 1, '{}', 'Portable camp kit.', 0, 3),
+('mid_camp_kit', 'Traveler Camp', 1, 1, 'item_standard', 1, 0, 1, '{}', 'Portable camp kit.', 0, 5),
+('pro_camp_kit', 'Traveler Camp', 1, 1, 'item_standard', 1, 0, 1, '{}', 'Portable camp kit.', 0, 8);
